94d2ccd603cd32467eb9a3ddcec7ddfe73cf9f26
Sébastien Thuret <contact@sebastien-thuret.fr>
2021-04-16 14:48:00 +0200

    Add nelmio doc api

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
