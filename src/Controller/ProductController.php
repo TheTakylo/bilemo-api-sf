10029c09df705d83595c567d3449586f8ecc4cd6
Sébastien Thuret <contact@sebastien-thuret.fr>
2021-04-29 14:14:50 +0200

    Add docs

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use App\Repository\ProductRepository;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use OpenApi\Annotations as OA;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,10 +15,23 @@ use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 /**
  * @IsGranted("ROLE_USER")
  * @Route("/api/products")
+ * @OA\Tag(name="Product")
  */
 class ProductController extends AbstractController
 {
     /**
+     * @OA\Get(
+     *   operationId="get-collection-product",
+     *   summary="Get list of products",
+     * @OA\Response(
+     *     response="200",
+     *     description="All products",
+     *     @OA\JsonContent(
+     *        type="array",
+     *        @OA\Items(ref=@Model(type=Product::class))
+     *     )
+     *   )
+     * )
      * @Route("", methods={"GET"})
      */
     public function getCollection(ProductRepository $productRepository, NormalizerInterface $normalizer): Response
@@ -30,6 +46,23 @@ class ProductController extends AbstractController
     }
 
     /**
+     * @OA\Get(
+     *   operationId="get-product",
+     *   summary="Get a product",
+     * @OA\Parameter(
+     *     name="id",
+     *     in="path",
+     *     @OA\Schema(type="integer")
+     * ),
+     *  @OA\Response(
+     *     response="200",
+     *     description="A product",
+     *     @OA\JsonContent(
+     *        type="object",
+     *        @OA\Items(ref=@Model(type=Product::class, groups={"read"}))
+     *     )
+     *   )
+     * )
      * @Route("/{id<\d+>}", methods={"GET"}, name="product_getitem")
      */
     public function getItem(int $id, ProductRepository $productRepository): Response
